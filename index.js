b901d11d592c666f0b6cee0f532c10029b0ef80b
anushkadeshpande <anushkaa5000@gmail.com>
2023-11-18 19:36:29 +0530

    fix: Corrected imports

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
-import { typeDefs } from './schema'
-import db from './db'
+import { typeDefs } from './schema.js'
+import db from './db.js'
 
 const resolvers = {
     Query: {
