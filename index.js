511d3a1c24805295d70404292494b569ed05a140
anushkadeshpande <anushkaa5000@gmail.com>
2023-11-18 19:33:14 +0530

    feat: Added resolver functions

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
+import { typeDefs } from './schema'
+import db from './db'
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        },
+
+        reviews() {
+            return db.reviews
+        },
+
+        authors() {
+            return db.authors
+        }
+    }
+}
 
 // server setup
-
 const server = new ApolloServer({
-    // typedefs  --- definitions of types of data
-    // resolvers 
-
+    typeDefs,
+    resolvers 
 })
 
 const { url } = await startStandaloneServer(server, {
